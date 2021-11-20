f99bb5757aedae4dffeced56a2340c3bb5f464d1
ThaisSLino <thaisslino@gmail.com>
2021-11-19 23:01:47 -0400

    correcoes

--- JavaScript/aula4/ex4.js
+++ JavaScript/aula4/ex4.js
@@ -7,40 +7,40 @@ let plano = true;
 // let descricaoPlano = "Ouro"
 let imc = peso/(altura*altura);
 
-let saida = nome + 'tem' + idade + 'anos' + 'e seu índice de massa corporal é de' + 'imc.toFixed(2)';
+let saida = nome + 'tem' + idade + 'anos' + 'e seu índice de massa corporal é de ' + imc.toFixed(2);
 console.log(saida); 
 
 //Pessoa 2
-let nome = "Carlos de Souza";
-let idade = 28;
-let peso = 80.1;
-let altura = 1.76;
-let plano = true;
+nome = "Carlos de Souza";
+idade = 28;
+peso = 80.1;
+altura = 1.76;
+plano = true;
 // let descricaoPlano = "Diamante"
-let imc = peso/(altura*altura);
+imc = peso/(altura*altura);
 
-let saida = nome + 'tem' + idade + 'anos' + 'e seu índice de massa corporal é de' + 'imc.toFixed(2)';
+saida = nome + 'tem' + idade + 'anos' + 'e seu índice de massa corporal é de ' + imc.toFixed(2);
 console.log(saida); 
 
 // Pessoa 3
-let nome = "Aline Ferreira";
-let idade = 33;
-let peso = 63.7;
-let altura = 1.53;
-let plano = false;
-let imc = peso/(altura*altura);
+nome = "Aline Ferreira";
+idade = 33;
+peso = 63.7;
+altura = 1.53;
+plano = false;
+imc = peso/(altura*altura);
 
-let saida = nome + 'tem' + idade + 'anos' + 'e seu índice de massa corporal é de' + 'imc.toFixed(2)';
+saida = nome + 'tem' + idade + 'anos' + 'e seu índice de massa corporal é de ' + imc.toFixed(2);
 console.log(saida); 
 
 //Pessoa 4
-let nome = "Ana Paula";
-let idade = 26;
-let peso = 55.0;
-let altura = 1.62;
-let plano = true;
+nome = "Ana Paula";
+idade = 26;
+peso = 55.0;
+altura = 1.62;
+plano = true;
 // let descricaoPlano = "Prata"
-let imc = peso/(altura*altura);
+imc = peso/(altura*altura);
 
-let saida = nome + 'tem' + idade + 'anos' + 'e seu índice de massa corporal é de' + 'imc.toFixed(2)';
+saida = nome + 'tem' + idade + 'anos' + 'e seu índice de massa corporal é de ' + imc.toFixed(2);
 console.log(saida); 
